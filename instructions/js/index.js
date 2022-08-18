28ae473f64a3bb6866fff2c5e4c68cba115195eb
VanessaSR <103793183+vsrodriguez3@users.noreply.github.com>
2022-08-18 16:46:09 -0400

    Added an HTML form with message list and remove button

--- instructions/js/index.js
+++ instructions/js/index.js
@@ -12,3 +12,40 @@ mySkills.forEach((item) => {
     li.innerText = item;
     skillsList.appendChild(li);
 });
+
+let messageForm = document.getElementsByName("leave_message")
+messageForm[0].addEventListener('submit', function(event) {
+    event.preventDefault()
+    const userName = event.target.name.value;
+    const userEmail = event.target.email.value;
+    const userMessage = event.target.message.value;
+    console.log(userName + "\n" + userEmail + "\n" + userMessage);
+    let messageSection = document.getElementById("messages");
+    let messageList = document.getElementById("messages").querySelector("ul");
+    let newMessage = document.createElement("li");
+    newMessage.innerHTML = '<a href="mailto:">' + event.target.name.value + '</a>' + ' <span>wrote: ' + event.target.message.value + '</span>  '
+    messageList.appendChild(newMessage);
+
+    const removeButton = document.createElement("button");
+    removeButton.type = "button";
+    removeButton.innerText = "Remove";
+    removeButton.addEventListener('click', function(e) {
+        let entry = removeButton.parentElement;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+
+    messageForm[0].reset();
+});
+
+/*let removeButton = document.createElement("button");
+removeButton.type = "button";
+removeButton.innerText = "remove";
+removeButton.addEventListener('click', function(e) {
+    let entry = removeButton.parentnode;
+    entry.remove();
+});
+
+newMessage.appendChild(removeButton);*/
+
